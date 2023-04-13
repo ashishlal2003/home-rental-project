300a720b2487c159aa9249fcbdde06f0796733bf
Ashish Lal <ashishlal2003@gmail.com>
2023-04-13 23:48:45 +0530

    pakka final

--- SQL Queries/setup.sql
+++ SQL Queries/setup.sql
@@ -18,7 +18,8 @@ Alter table staff add primary key(staff_number);
 
 
 
-CREATE INDEX idx_branch_number ON staff(branch_number);
+-- CREATE INDEX idx_branch_number ON staff(branch_number);
+CREATE INDEX idx_staff_name ON staff(full_name);
 
 
 create table prop_registration(p_no varchar(100),
@@ -33,9 +34,11 @@ tel_no varchar(15),
 business_type varchar(100),
 cont_no varchar(15),
 staff_manage varchar(100),
-branch varchar(100));
+branch varchar(100),
+city varchar(100));
 
 Alter table prop_registration add primary key (p_no);
+Alter table prop_registration add foreign key (staff_manage) references staff(full_name);
 
 create table payment_details(monthly_rent int,
 payment_method varchar(100),
@@ -48,32 +51,12 @@ duration varchar(100));
 
 create table branch_office(branch_number varchar(4),
 branch_address varchar(100),
-telephone_number varchar(100));
+telephone_number varchar(100),
+city varchar(100));
 
 Alter table branch_office add primary key (branch_number);
 Alter table staff add foreign key (branch_number) references branch_office(branch_number);
 
-
-create table property(property_no  varchar(100),
-property_type  varchar(100),
-rooms int,
-rent int,
-property_address  varchar(100),
-managing_staff_no  varchar(100),
-owner_no  varchar(100),
-branch_no  varchar(100));
-
-Alter table property add foreign key (managing_staff_no) references staff(staff_number);
-Alter table property add foreign key (property_no) references prop_registration(p_no);
-
-
-
-create table staff_details(staff_number varchar(100),
-full_name varchar(100),
-position varchar(100),
-branch_number varchar(100));
-
-Alter table staff_details add primary key(staff_number);
 
 create table client_registration(client_no varchar(100),
 name varchar(100),
@@ -94,12 +77,6 @@ comments varchar(100));
 Alter table comments add primary key (client_no);
 Alter table comments add foreign key (client_no) references client_registration( client_no);
 
-create table details(property_no varchar(100),
-type varchar(100),
-rent int,
-property_address varchar(100));
-
-Alter table details add primary key (property_no);
 
 create table lease_table(client_number varchar(100),
 full_name varchar(100),
@@ -108,4 +85,4 @@ property_address varchar(100));
 
 Alter table lease_table add primary key (property_number);
 Alter table lease_table add foreign key (client_number) references client_registration(client_no);
-Alter table lease_table add foreign key (property_number) references property(property_no);
+Alter table lease_table add foreign key (property_number) references prop_registration(p_no);
